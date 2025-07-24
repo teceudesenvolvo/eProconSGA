2b91437bcc42dd93890610f4047b783e53a1fb72
teceudesenvolvo <tecnologia@eudesenvolvo.com>
2025-07-24 15:15:45 -0300

    melhorias

--- src/screens/homeDashboard.js
+++ src/screens/homeDashboard.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
-import { GoBook, GoCircleSlash, GoCalendar, GoFile  } from 'react-icons/go';
+import { GoBook, GoCircleSlash, GoCalendar, GoFile } from 'react-icons/go';
 import SlideFeacures from '../componets/slideFeactures';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
 // Imagens
-import imgHeader from '../assets/banner-header.png'
-
+import imgHeader from '../assets/plenario.png' // Esta imagem será usada no carrossel ou como um banner genérico
 
 function HomeDashboard() {
     const navigate = useNavigate();
@@ -19,7 +18,7 @@ function HomeDashboard() {
                 } else {
                     navigate('/perfil');
                 }
-                }
+            }
             else {
 
             }
@@ -32,28 +31,53 @@ function HomeDashboard() {
     return (
         <div className='App-header'>
             <div className='Home-Dach'>
-                <img src={imgHeader} className='imgHeader' alt='banner imagem inicio' />
+                {/* Novo Header baseado na imagem */}
+                <div className="header-content">
+                    <div className="header-text">
+                        <h1>Acessar o PROCON agora é online e fácil de usar!</h1>
+                        <p>Seja atendido fácilemente por nosso portal online.</p>
+                        {/* Botões de Login/Cadastro (mantidos) */}
+                        <div className='btnsHome'>
+                            <a href='/login' className='btnHome'>Login</a>
+                            <a href='/register' className='btnHome'>Cadastrar</a>
+                        </div>
+                    </div>
+                    <div className="header-image-container">
+                        {/*  */}
+                        <img src={imgHeader} alt="Médico sorrindo" className="header-image" />
+                    </div>
+                </div>
+
+                {/* Seção de botões de ação (mantida, mas com margem ajustada) */}
                 <div className='btnsHome'>
-                    <a href='/login' className='btnHome'>Login</a>
-                    <a href='/register' className='btnHome'>Cadastrar</a>
+                    <div className='btnLargeHome'>
+                        <a href='/registrar-reclamacao' >
+                            <GoCircleSlash className="iconHomeBtn" /><br />Realizar Reclamação
+                        </a>
+                    </div>
+
+                    <div className='btnLargeHome'>
+                        <a href='/meus-atendimentos'>
+                            <GoCalendar className="iconHomeBtn" /><br />Agendar Atendimento
+                        </a>
+                    </div>
+
+                    <div className='btnLargeHome'>
+                        <a href='/codigo-de-defesa-do-consumidor'>
+                            <GoFile className="iconHomeBtn" /><br />Código Defesa do Consumidor
+                        </a>
+                    </div>
+
+                    <div className='btnLargeHome'>
+                        <a href='https://www.consumidor.gov.br/' target="_blank" rel="noopener noreferrer">
+                            <GoBook className="iconHomeBtn" /><br />Consumidor Gov
+                        </a>
+                    </div>
                 </div>
-                <div className='header-Dach-div'>
-                    <h1>Destaques</h1>
-                </div>
-                <div className='btnsHome'>
-                    <a href='/registrar-reclamacao' className='btnLargeHome'>
-                        <GoCircleSlash className="iconHomeBtn" /><br />Realizar Reclamação
-                    </a>
-                    <a href='/meus-atendimentos' className='btnLargeHome'>
-                        <GoCalendar className="iconHomeBtn" /><br />Agendar Atendimento
-                    </a>
-                    <a href='/codigo-de-defesa-do-consumidor' className='btnLargeHome'>
-                        <GoFile  className="iconHomeBtn" /><br />Codigo Defesa do Consumidor
-                    </a>
-                    <a className='btnLargeHome' href='https://www.consumidor.gov.br/' target="_blank" rel="noopener noreferrer">
-                        <GoBook className="iconHomeBtn" /><br />Consumidor Gov
-                    </a>
-                </div>
+
+
+
+                {/* Seção "Nossos Vereadores" e Carrossel (mantidos) */}
                 <div className='header-Dach-div'>
                     <h1>Nossos Vereadores</h1>
                 </div>
